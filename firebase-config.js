895e0d2db3a25dd5aed85a28c446431659f5c8d2
joissihotang18-glitch <joissihotang18@gmail.com>
2026-06-16 00:08:03 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+firebase-config.js
+const firebaseConfig = {
+    apiKey: "ghp_123comSimulasiTokenGitHubFake12345", 
+    authDomain: "tugas-cybersecurity-simulasi.firebaseapp.com",
+    projectId: "tugas-cybersecurity-simulasi"
+};
